57bc924d2227975dab12deb3efbe3d576164e695
hidden2000 <93968918+hidden2000@users.noreply.github.com>
2022-01-05 23:05:13 +0530

    Create N x N chessboard.cpp

---
+++ N x N chessboard.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+class Solution {
+  public:
+    long long squaresInChessBoard(long long N) {
+        // code here
+        // the number of squares will be (N*(N+1)*((2*N)+1))/6
+        long long x = N*(N+1)*((2*N)+1);
+        return x/6;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.squaresInChessBoard(N) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
